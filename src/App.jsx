0b7a59dfa412842ee32f771656381daed359b856
alfgow <alfonso@villanuevagarcia.com>
2024-03-30 01:24:33 -0600

    Test Componente Guitarra

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Guitar from "./components/Guitar";
 import Header from "./components/Header";
 
 function App() {
@@ -10,42 +11,7 @@ function App() {
 				</h2>
 
 				<div className="row mt-5">
-					<div className="col-md-6 col-lg-4 my-4 row align-items-center">
-						<div className="col-4">
-							<img
-								className="img-fluid"
-								src="./public/img/guitarra_01.jpg"
-								alt="imagen guitarra"
-							/>
-						</div>
-						<div className="col-8">
-							<h3 className="text-black fs-4 fw-bold text-uppercase">
-								Lukather
-							</h3>
-							<p>
-								Lorem ipsum,
-								dolor sit amet
-								consectetur
-								adipisicing
-								elit. Sit quae
-								labore odit
-								magnam in autem
-								nesciunt, amet
-								deserunt
-							</p>
-							<p className="fw-black text-primary fs-3">
-								$299
-							</p>
-							<button
-								type="button"
-								className="btn btn-dark w-100"
-							>
-								Agregar al
-								Carrito
-							</button>
-						</div>
-					</div>
-
+					<Guitar />
 					<div className="col-md-6 col-lg-4 my-4 row align-items-center">
 						<div className="col-4">
 							<img
