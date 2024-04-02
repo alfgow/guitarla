350850389523566d51772e544513be2eedfcbca7
alfonsovillanueva <alfonso@villanuevagarcia.com>
2024-04-01 23:53:27 -0600

    Clase 66: Mostrando Info Guitarra

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,24 +1,21 @@
-function Guitar() {
+function Guitar({ guitar }) {
+	const { id, name, description, price, image } = guitar;
 	return (
 		<div className="col-md-6 col-lg-4 my-4 row align-items-center">
 			<div className="col-4">
 				<img
 					className="img-fluid"
-					src="./public/img/guitarra_01.jpg"
+					src={`/img/${image}.jpg`}
 					alt="imagen guitarra"
 				/>
 			</div>
 			<div className="col-8">
 				<h3 className="text-black fs-4 fw-bold text-uppercase">
-					alfgow
+					{name}
 				</h3>
-				<p>
-					Lorem ipsum, dolor sit amet consectetur
-					adipisicing elit. Sit quae labore odit
-					magnam in autem nesciunt, amet deserunt
-				</p>
+				<p>{description}</p>
 				<p className="fw-black text-primary fs-3">
-					$299
+					${price}
 				</p>
 				<button
 					type="button"
