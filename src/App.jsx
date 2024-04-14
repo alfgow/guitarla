70234922dddb3e59e6575d2e050de4ac367dfa6a
alfgow <alfonso@villanuevagarcia.com>
2024-04-14 00:18:09 -0600

    Personal Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,19 @@
-import { useEffect, useState } from "react";
 import Guitar from "./components/Guitar";
 import Header from "./components/Header";
-import { db } from "./data/db";
+import { useCart } from "./hooks/useCart";
 
 function App() {
-	const inicialCart = () => {
-		const localStorageCart = localStorage.getItem("cart");
-		return localStorageCart ? JSON.parse(localStorageCart) : [];
-	};
-	const [data] = useState(db);
-	const [cart, setCart] = useState(inicialCart);
-
-	useEffect(() => {
-		localStorage.setItem("cart", JSON.stringify(cart));
-	}, [cart]);
-
-	function addToCart(item) {
-		const itemExists = cart.findIndex(
-			(guitar) => guitar.id === item.id
-		);
-		if (itemExists >= 0) {
-			const updatedCart = [...cart];
-			updatedCart[itemExists].quantity++;
-			setCart(updatedCart);
-		} else {
-			item.quantity = 1;
-			setCart([...cart, item]);
-		}
-	}
-
-	function removeFromCart(id) {
-		setCart((prevCart) =>
-			prevCart.filter((guitar) => guitar.id !== id)
-		);
-	}
-
-	function clearCart() {
-		setCart([]);
-	}
-
-	function increaseQuantity(id) {
-		const updatedCart = cart.map((item) => {
-			if (item.id === id && item.quantity < 5) {
-				return { ...item, quantity: item.quantity + 1 };
-			}
-			return item;
-		});
-		setCart(updatedCart);
-	}
-
-	function dicreaseQuantity(id) {
-		const updatedCart = cart.map((item) => {
-			if (item.id === id && item.quantity > 1) {
-				return { ...item, quantity: item.quantity - 1 };
-			}
-
-			return item;
-		});
-		setCart(updatedCart);
-	}
+	const {
+		data,
+		cart,
+		addToCart,
+		removeFromCart,
+		clearCart,
+		increaseQuantity,
+		dicreaseQuantity,
+		isEmpty,
+		cartTotal,
+	} = useCart();
 
 	return (
 		<>
@@ -68,6 +23,8 @@ function App() {
 				increaseQuantity={increaseQuantity}
 				dicreaseQuantity={dicreaseQuantity}
 				clearCart={clearCart}
+				isEmpty={isEmpty}
+				cartTotal={cartTotal}
 			/>
 			<main className="container-xl mt-5">
 				<h2 className="text-center">
